81166d19bc7036cc8b10d98c99baa122e96119d7
SJ <sage.c.jung@gmail.com>
2025-02-25 20:22:06 -0500

    Made images public

--- components/character.vue
+++ components/character.vue
@@ -5,7 +5,7 @@
 <template>
         <div class="relative w-full h-auto flex items-center justify-center">
             <div class="w-1/3 h-auto">
-                <img class="object-contain" src="../pet-1.png" alt="A little cute white blob of an animal with round ears and a cute face.">
+                <img class="object-contain" src="../public/img/pet-1.png" alt="A little cute white blob of an animal with round ears and a cute face.">
             </div>
         </div>
 </template>
